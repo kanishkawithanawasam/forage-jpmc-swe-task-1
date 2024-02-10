485ecd566f36464e362af7fb83476a6b7fbfa540
Kanishka Withanawasam <psykw3@nottingham.ac.uk>
2024-02-10 18:09:58 +0000

    Addition: getDataPoint() unit tests.

--- client_test.py
+++ client_test.py
@@ -7,7 +7,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+
+    # Expected datapoints
+    expectedDataPoint1 = (quotes[0]["top_bid"]["price"]+quotes[0]["top_ask"]["price"])/2
+    expectedDataPoint2 = (quotes[1]["top_bid"]["price"]+quotes[1]["top_ask"]["price"])/2
+
+    self.assertEqual(getDataPoint(quotes[0])[3],expectedDataPoint1)
+    self.assertEqual(getDataPoint(quotes[1])[3],expectedDataPoint2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
